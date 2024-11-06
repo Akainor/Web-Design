562dc27840c040c9ce1cc4781d31b66f672034e9
Akainor <akainor@hotmail.com>
2024-11-06 08:26:53 -0300

    WOW

---
+++ Prova_AC2/main.js
@@ -0,0 +1,19 @@
+let itens = [];
+function adicionarItem() {
+    const texto = document.querySelector('#input').value;
+    itens.push(texto);
+    atualizarLista();
+}
+function apagar(index) {
+    itens.splice(index, 1);
+    atualizarLista();
+}
+function atualizarLista() {
+    const list = document.querySelector('#list');
+    list.innerHTML = '';
+    itens.forEach((item, index) => {
+        const novoItem = document.createElement('li'); novoItem.innerHTML = '<div class="card" style="width: 18rem;"><img src="https://picsum.photos/600/400?radom(' + index + ')" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' + item + '</h5><p class="card-text"></p><button class="btn btn-primary" onclick="apagar(' + index + ')">Apagar</button></div></div>';
+        list.appendChild(novoItem);
+    });
+    document.querySelector('#input').value = "";
+}
