28a890fb47c19646d2a508dd8bd630dc5e379f49
Akainor <akainor@hotmail.com>
2024-11-06 08:49:29 -0300

    Fix

--- Prova_AC2/main.js
+++ Prova_AC2/main.js
@@ -12,7 +12,7 @@ function atualizarLista() {
     const list = document.querySelector('#list');
     list.innerHTML = '';
     itens.forEach((item, index) => {
-        const novoItem = document.createElement('li'); novoItem.innerHTML = '<div class="card" style="width: 18rem;"><img src="https://picsum.photos/600/400?radom(' + index + ')" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' + item + '</h5><p class="card-text"></p><button class="btn btn-primary" onclick="apagar(' + index + ')">Apagar</button></div></div>';
+        const novoItem = document.createElement('li'); novoItem.innerHTML = '<div class="col"><div class="card h-100" style="width: 18rem;"><img src="https://picsum.photos/600/400?radom(' + index + ')" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' + item + '</h5><p class="card-text"></p><button class="btn btn-primary" onclick="apagar(' + index + ')">Apagar</button></div></div></div>';
         list.appendChild(novoItem);
     });
     document.querySelector('#input').value = "";
